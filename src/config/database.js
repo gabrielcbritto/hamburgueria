4be96470d40eb1f40be41ec13987bcb91fe936d3
gabrielcbritto <gabrielcbritto@gmail.com>
2023-05-22 19:06:25 -0300

    add sequelize and some folders

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'codeburger',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
+
+user
