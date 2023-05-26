6019387801868951551687a562ea20dc39985555
gabrielcbritto <gabrielcbritto@gmail.com>
2023-05-25 21:36:47 -0300

    users model created test postbird message nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
+const { v4 } = require('uuid')
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'ON!!!' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'test',
+    email: 'test@gmail.com',
+    password_hash: '123123',
+  })
+  return response.json(user)
 })
 
 export default routes
